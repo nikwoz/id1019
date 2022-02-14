73c90e9e27623f4a415828356562d3f47ec6c919
nikwoz <77621167+nikwoz@users.noreply.github.com>
2022-02-14 22:23:25 +0100

    Update stream.ex

--- A6/stream.ex
+++ A6/stream.ex
@@ -1,12 +1,29 @@
 defmodule Str do
-	def z(int) do 
-		{int, fun}
+
+#sequence of natural numbers:
+	def z() do 
+		z(0)
+	end	
+	def z(n) do 
+		{n, fn() -> z(n+1) end}
 	end
 
-	def fun do 
-		{int +1, fun}
+#filtering sequence: (odd/even) 
+	def filter(fun, fil) do
+		{n, f} = fun.() 
+		case rem(n, fil) do
+				#returns a function for the next odd 
+			0 -> filter(f, fil)
+				#returns the odd and a function for the next
+			_ -> {n, fn() ->filter(f, fil) end}
+		end
 	end
 
+#defining the sieves: 	
+	def sieves(fun, p) do 
+		{n, f} = filter(z.(3), 2)
+		{n, sieves(filter(f, n), n)}
+	end
 
 
 end	
